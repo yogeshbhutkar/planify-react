9224d34da0a228eb3d20610f41ed3a0ba720c9fe
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-05-23 20:31:03 +0530

    Removed warnings from the console

--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -1,9 +1,4 @@
-import { useEffect, useState } from "react";
-import { getAllTasks } from "../utils/apiUtils";
-
 function Products() {
-  const [tasks, setTasks] = useState();
-
   return (
     <div className="inline-block w-screen p-16">
       <p className="text-gray-300 font-bold text-2xl">Overview of the tasks</p>
